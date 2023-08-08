17dce2f87e6ecc51d8f30e700fe06418a51adca9
mvmfaria <marcosfaria03@hotmail.com>
2023-08-08 20:40:37 -0300

    feat: o arquivo que é selecionado agora pode ser lido e mostrado na tabela.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,19 +1,12 @@
-//Global variables.
+
 let result = "";
-
-// const fetchResults = async () => {
-//   const response = await fetch('http://localhost:3333/results')
-//   const results = await response.json()
-//   return results;
-// };
-
-//Execute crawler.
 document.getElementById('link-button').addEventListener('click', async () => {
   const url = document.getElementById('link-input').value;
+  document.getElementById('link-input').value = "";
   document.getElementById("loading-text").innerHTML = "Em alguns instantes já será possível fazer o download do arquivo."
-
+  
   document.getElementById('download-button').disabled = true;
-
+  
   const response = await fetch('http://localhost:3333/results', {
     method: 'POST',
     headers: {
@@ -21,31 +14,100 @@ document.getElementById('link-button').addEventListener('click', async () => {
     },
     body: JSON.stringify({ url }),
   });
-
+  
   if (!response.ok) {
-      throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok');
   }
-
+  
   result = await response.json();
   document.getElementById('download-button').disabled = false;
   document.getElementById("loading-text").innerHTML = "Extração concluída!"
 });
 
-//Download a json file with content provide by crawler.
+/**
+ * Download a json file with content provide by crawler.
+*/
 document.getElementById('download-button').addEventListener('click', () => {
-
-  console.log("O arquivo já pode ser baixado.")
+  
+  document.getElementById('download-button').disabled = true;
   
   const jsonData = JSON.stringify(result, null, " ");
-
+  
   const blob = new Blob([jsonData], { type: 'application/json' });
-
+  
   const url = URL.createObjectURL(blob);
-
+  
   const a = document.createElement('a');
   a.href = url;
   a.download = 'resultado.json';
   a.click();
-
+  
   URL.revokeObjectURL(url);
 })
+
+/**
+ * Creating table stuff.
+*/
+
+const tbody = document.querySelector('tbody');
+const createElement = (tag, innerText = "") => {
+  const element = document.createElement(tag);
+  element.innerText = innerText;
+  
+  return element;
+}
+
+const task = {
+  link: "A random link",
+  textoAlternativo: "That's definetely a random link",
+  rotulo: "0"
+}
+
+const createRow = (task) => {
+  const { link, textoAlternativo, rotulo } = task;
+  const tr = createElement('tr');
+  const tdLink = createElement('td', link);
+  const tdTextoAlternativo = createElement('td', textoAlternativo);
+  const tdRotulo = createElement('td', rotulo)
+  const select = createElement('select')
+  const option_zero = createElement('option', 0)
+  const option_one = createElement('option', 1)
+
+  tr.appendChild(tdLink);
+  tr.appendChild(tdTextoAlternativo);
+  tr.appendChild(tdRotulo);
+  select.appendChild(option_zero);
+  select.appendChild(option_one);
+  tdRotulo.appendChild(select);
+  tbody.appendChild(tr);
+}
+
+document.getElementById("save-results-button").addEventListener('click', () => {
+  createRow(task);
+});
+
+/**
+ * Upload a json file from user computer.
+*/
+const inputFile = document.querySelector('#input-file')
+inputFile.addEventListener('change', function() {
+  const arquivo = this.files[0];
+  const leitor = new FileReader();
+
+  leitor.addEventListener('load', function() {
+    readingFile(eval(leitor.result))
+  })
+
+  if (arquivo) {
+    leitor.readAsText(arquivo);
+  }
+
+});
+
+const readingFile = (result) => {
+  for (const element of result) {
+    for (const image of element.imgSLinks) {
+      createRow({link: image.imgUrl, textoAlternativo: image.imgAlt})
+    }
+  }
+}
